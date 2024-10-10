5db1245cf67d296384255aa8a1a8ba29c94862ec
Anderson <anderson.e2@gmail.com>
2024-10-09 23:08:17 -0300

    First commit - Landing Page Vue.js

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="hero">
     <div class="hero-content">
-      <img src="logo.png" alt="Logo" class="logo" />
+            <img src="@/assets/logo1.png" alt="Logo Grupo" class="logo" />
       <nav class="navbar">
         <ul>
           <li><a href="#">Home</a></li>
@@ -21,29 +21,39 @@
 
 <script>
 export default {
-  name: "HeroSection",
+  name: 'HeroSection',
   methods: {
     cadastrar() {
-      alert("Botão de cadastrar clicado!");
+      alert('Botão de cadastrar clicado!');
     }
   }
-};
+}
 </script>
 
 <style scoped>
 .hero {
   text-align: center;
-  padding: 50px;
 }
+
+.logo {
+  width: 100px;
+  margin-bottom: 20px;
+}
+
 .navbar ul {
   list-style: none;
+  padding: 0;
   display: flex;
   justify-content: center;
 }
+
 .navbar ul li {
-  margin: 0 15px;
+  margin: 0 10px;
 }
-.banner {
-  margin-top: 20px;
+
+.navbar ul li a {
+  text-decoration: none;
+  color: #5e20a4; /* Ajuste a cor conforme necessário */
+  font-weight: bold;
 }
 </style>
